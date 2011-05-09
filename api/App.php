61b69f574c1f80f4a640d4ab2eacc87fbda519cb
Robert Middleswarth <robert@middleswarth.net>
2011-05-08 22:50:29 -0400

    More updates

--- api/App.php
+++ api/App.php
@@ -15,12 +15,6 @@ class Cerb5blogRequiredWatchersEventListener extends DevblocksEventListenerExten
 			case 'cerb5blog.context_link.watcher':
 				$this->_workerAssigned($event);
 				break;
-			case 'ticket.reply.inbound':
-				//$this->_sendForwards($event, true);
-				break;
-			case 'ticket.reply.outbound':
-				//$this->_sendForwards($event, false);
-				break;
 		}
 	}
 
@@ -87,14 +81,10 @@ class Cerb5blogRequiredWatchersEventListener extends DevblocksEventListenerExten
 				$ticket->subject
         );
         
-        $body = sprintf("[Ticket Assignment #%s]: %s",
-				$ticket->mask,
-				$ticket->subject
-		);
 		$url_writer = DevblocksPlatform::getUrlService();
         $url = $url_writer->write(sprintf("c=display&mask=%s", $ticket->mask), true);
 
-        $body .= "\r\n" . $url;
+        $body = "##Url: " . $url;
         $body .= "\r\n" . $message->getContent();
 
 		CerberusMail::quickSend(
@@ -173,136 +163,6 @@ class Cerb5blogRequiredWatchersEventListener extends DevblocksEventListenerExten
 					
 		} catch(Exception $e) {
             echo "Task Email Notification failed to send<br>";
-		}
-	}
-        
-	private function _sendForwards($event, $is_inbound) {
-		@$ticket_id = $event->params['ticket_id'];
-		@$send_worker_id = $event->params['worker_id'];
-    	
-		$url_writer = DevblocksPlatform::getUrlService();
-		
-		$ticket = DAO_Ticket::get($ticket_id);
-
-		// (Action) Forward Email To:
-		
-		// Sanitize and combine all the destination addresses
-		$context_workers = CerberusContexts::getWorkers(CerberusContexts::CONTEXT_TICKET, $ticket->id);
-		if(!is_array($context_workers))
-			return;
-		foreach($context_workers as $next_worker) {
-			$notify_emails = $next_worker->email;
-			
-			if(empty($notify_emails))
-				continue;
-		}
-
-		// [TODO] This could be more efficient
-		$messages = DAO_Message::getMessagesByTicket($ticket_id);
-		$message = end($messages); // last message
-		unset($messages);
-		$headers = $message->getHeaders();
-			
-			// The whole flipping Swift section needs wrapped to catch exceptions
-		try {
-			$settings = DevblocksPlatform::getPluginSettingsService();
-			$reply_to = $settings->get('cerberusweb.core',CerberusSettings::DEFAULT_REPLY_FROM,CerberusSettingsDefaults::DEFAULT_REPLY_FROM);
-			
-			// See if we need a group-specific reply-to
-			if(!empty($ticket->team_id)) {
-				@$group_from = DAO_GroupSettings::get($ticket->team_id, DAO_GroupSettings::SETTING_REPLY_FROM, '');
-				if(!empty($group_from))
-					$reply_to = $group_from;
-			}
-			
-			$sender = DAO_Address::get($message->address_id);
-	
-			$sender_email = strtolower($sender->email);
-			$sender_split = explode('@', $sender_email);
-	
-			if(!is_array($sender_split) || count($sender_split) != 2)
-				return;
-	
-			// If return-path is blank
-			if(isset($headers['return-path']) && $headers['return-path'] == '<>')
-				return;
-				
-				// Ignore bounces
-			if($sender_split[0]=="postmaster" || $sender_split[0] == "mailer-daemon")
-				return;
-			
-			// Ignore autoresponses autoresponses
-			if(isset($headers['auto-submitted']) && $headers['auto-submitted'] != 'no')
-				return;
-				
-			// Attachments
-			$attachments = $message->getAttachments();
-			$mime_attachments = array();
-			if(is_array($attachments))
-			foreach($attachments as $attachment) {
-				if(0 == strcasecmp($attachment->display_name,'original_message.html'))
-					continue;
-				
-				$attachment_path = APP_STORAGE_PATH . '/attachments/'; // [TODO] This is highly redundant in the codebase
-				if(!file_exists($attachment_path . $attachment->filepath))
-					continue;
-				
-				$attach = Swift_Attachment::fromPath($attachment_path . $attachment->filepath);
-				if(!empty($attachment->display_name))
-					$attach->setFilename($attachment->display_name);
-				$mime_attachments[] = $attach;
-			}
-	    	
-			// Send copies
-			$mail_service = DevblocksPlatform::getMailService();
-			$mailer = $mail_service->getMailer(CerberusMail::getMailerDefaults());
-				
-			$mail = $mail_service->createMessage(); /* @var $mail Swift_Message */
-			$mail->setTo(array($notify_emails));
-			$mail->setFrom(array($sender->email));
-			$mail->setReplyTo($reply_to);
-			$mail->setReturnPath($reply_to);
-			$mail->setSubject(sprintf("[RW: %s #%s]: %s",
-				($is_inbound ? 'inbound' : 'outbound'),
-				$ticket->mask,
-				$ticket->subject
-			));
-
-			$hdrs = $mail->getHeaders();
-			
-			if(null !== (@$msgid = $headers['message-id'])) {
-				$hdrs->addTextHeader('Message-Id',$msgid);
-			}
-				
-			if(null !== (@$in_reply_to = $headers['in-reply-to'])) {
-			    $hdrs->addTextHeader('References', $in_reply_to);
-			    $hdrs->addTextHeader('In-Reply-To', $in_reply_to);
-			}
-			
-			$hdrs->addTextHeader('X-Mailer','Cerberus Helpdesk (Build '.APP_BUILD.')');
-			$hdrs->addTextHeader('Precedence','List');
-			$hdrs->addTextHeader('Auto-Submitted','auto-generated');
-			
-			$mail->setBody($message->getContent());
-	
-			// Send message attachments with watcher
-			if(is_array($mime_attachments))
-			foreach($mime_attachments as $mime_attachment) {
-				$mail->attach($mime_attachment);
-			}
-				
-			$result = $mailer->send($mail);
-		} catch(Exception $e) {
-			if(!empty($message_id)) {
-				$fields = array(
-					DAO_MessageNote::MESSAGE_ID => $message_id,
-					DAO_MessageNote::CREATED => time(),
-					DAO_MessageNote::WORKER_ID => 0,
-					DAO_MessageNote::CONTENT => 'Exception thrown while sending watcher email: ' . $e->getMessage(),
-					DAO_MessageNote::TYPE => Model_MessageNote::TYPE_ERROR,
-				);
-				DAO_MessageNote::create($fields);
-			}
 		}
 	}
 };
